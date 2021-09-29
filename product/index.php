319609c3ba8f3e17ac69f137489471150dab57ef
oktoala <okta.yoga.syah@gmail.com>
2021-09-29 21:50:03 +0800

    Done Product UI

--- product/index.php
+++ product/index.php
@@ -13,33 +13,69 @@
     <?php include '../component/header.html'; ?>
     <main class="main container">
         <section class="section1 center">
-            <div class="grid-container">
+            <?php if (!empty($_GET)) : ?>
                 <?php
-                if (!empty($_GET)) {
-                    print_r($_GET);
-                } else {
-                    $json_data = file_get_contents('https://fakestoreapi.com/products');
-                    $response_data = json_decode($json_data);
-                    // print_r($response_data[0]->id);
-
-                    foreach ($response_data as $response) {
-                        echo "<a href='?id=$response->id'>";
-                        echo "<article id='$response->id' class='grid-item'>";
-                        echo "<img class='grid-img' src='$response->image' />";
-                        echo  "<div class='grid-value'>";
-                        echo "<div class='grid-title'>$response->title</div>";
-                        echo "<div class='grid-price'>\$$response->price</div>";
-                        echo  "</div>";
-                        echo "</article>";
-                        echo "</a>";
-                    }
-                }
+                $id = $_GET["id"];
+                $json_data = file_get_contents("https://fakestoreapi.com/products/$id");
+                $response_data = json_decode($json_data);
+                $discount = 15 / 100;
+                $real_price =  number_format($response_data->price / (1 - $discount), 2);
                 ?>
+                <div class='product'>
+                    <img src='<?= $response_data->image ?>' />
+                    <div class="product-item">
+                        <div class='product-desc'>
+                            <div class='product-title'>
+                                <?= $response_data->title ?>
+                            </div>
+                            <div class='product-price'>
+                                <div class="real-price">
+                                    $<?= $real_price ?>
+                                </div>
+                                <div class="discount-price">
+                                    $<?= $response_data->price ?>
+                                </div>
+                                <div class="discount-value">15% OFF</div>
+                            </div>
+                            <div class="product-description">
+                                <h3>Description:</h3>
+                                <p><?= $response_data->description ?></p>
+                            </div>
+                        </div>
+                        <div class="product-cart">
+                            <?php include('../icon/add_shopping_cart.svg') ?>
+                            <span>
+                                Add to Cart
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            <?php else : ?>
+                <?php
+                $json_data = file_get_contents('https://fakestoreapi.com/products');
+                $response_data = json_decode($json_data);
+                ?>
+                <div class='grid-container'>
+                    <?php foreach ($response_data as $response) : ?>
+                        <a href='?id=<?= $response->id ?>'>
+                            <article id='<?= $response->id ?>' class='grid-item'>
+                                <img class='grid-img' src='<?= $response->image ?>' />
+                                <div class='grid-value'>
+                                    <div class='grid-title'><?= $response->title ?></div>
+                                    <div class='grid-price'>$<?= $response->price ?></div>
+                                </div>
+                            </article>
+                        </a>
+                    <?php endforeach ?>
+                </div>
 
-            </div>
+            <?php endif ?>
+
         </section>
     </main>
     <script>
-        document.querySelector('li:nth-child(2) > a').style.color = 'rgb(250, 217, 33)';
+        const nav = document.querySelector('li:nth-child(2) > a');
+        nav.style.color = 'rgb(250, 217, 33)';
+        nav.style.fontWeight = 'bold';
     </script>
 </body>
